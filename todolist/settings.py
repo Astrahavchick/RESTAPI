017586d8567502b34219d0c558698d068adbcc80
Viktor <viktor1211200411@gmail.com>
2025-06-18 20:37:39 +0300

    Add middleware to disable CSRF checks for specific requests

--- todolist/settings.py
+++ todolist/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'todos.middleware.DisableCSRFMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
